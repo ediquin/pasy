b60b1fd331e3717019bc770f2a8ebfeaee5226d3
ediquin <34133493+ediquin@users.noreply.github.com>
2023-06-28 23:28:37 -0400

    Delete js directory

--- js/visit-thanks.js
+++
@@ -1,7 +0,0 @@
-console.log("hello visit thanks");
-
-const visitThanksHomeBtn = document.querySelector("#visitThanksHome");
-
-visitThanksHomeBtn.addEventListener("click", () => {
-  window.location.href = "../index.html";
-});
